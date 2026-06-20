8ac89df6e893328b912fb7ea2567c80074cfdbfd
marceldetemple-svg <marceldetemple@googlemail.com>
2026-06-20 17:14:56 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -3,8 +3,9 @@
 
 const SUPABASE_URL = "https://crcxnehpaysaktmluyck.supabase.co";
 
-
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNyY3huZWhwYXlzYWt0bWx1eWNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5NTU4NTIsImV4cCI6MjA5NzUzMTg1Mn0.6FA-KWC3vz3_hqJK8DBVRgP73ki6U_c5TpUMBeLj6TU";
+// HIER MUSS NUR NOCH DER ANON PUBLIC KEY AUS SUPABASE REIN.
+// Wichtig: NICHT den secret key nehmen.
+const SUPABASE_ANON_KEY = "HIER_DEN_ANON_PUBLIC_KEY_EINFUEGEN";
 
 const isSupabaseConfigured =
   SUPABASE_URL.startsWith("https://") &&
